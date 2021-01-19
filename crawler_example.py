45c15f60da1511c4cae811008e17488573bfdb73
Okkar Min <omin001@e.ntu.edu.sg>
2021-01-19 20:56:17 +0800

    ➕ Implement crawler_example.py

---
+++ crawler_example.py
@@ -0,0 +1,37 @@
+import requests
+import random
+from faker import Faker
+from time import sleep
+
+fake = Faker()
+
+
+def crawl_for_data():
+    # Crawling for data
+    data = {
+        "name": fake.name(),
+        "gender": random.choice(["Male", "Female", "Non-binary"]),
+        "age": random.randrange(1, 100),
+        "job": fake.job(),
+        "favourite_movie": fake.text(max_nb_chars=20)
+    }
+
+    return data
+
+
+def post_to_logstash(data_in_json):
+    logstash_http_endpoint = "http://54.255.247.142:5151"
+
+    requests.post(logstash_http_endpoint, json=data_in_json)
+
+
+while True:
+    try:
+        data = crawl_for_data()
+        print(data)
+
+        post_to_logstash(data)
+
+        sleep(1)
+    except Exception as e:
+        print(e)
