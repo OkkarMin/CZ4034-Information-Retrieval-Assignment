ab35b2285b09553be808cb3c39bfd8c868589dd5
Okkar Min <omin001@e.ntu.edu.sg>
2021-03-16 09:54:16 +0000

    Automated autoyapf fixes

--- twitter crawler/postData.py
+++ twitter crawler/postData.py
@@ -1,7 +1,8 @@
 import requests
-import json 
+import json
 
 cleaned_data_directory = "C:/Users/whate/Documents/AY2021S2/CZ4034 Information Retrieval/twitter stuff/CLEANED_DATA_2/"
+
 
 def post_to_logstash(data_in_json):
     logstash_http_endpoint = "http://irgroup13.ml:5151"
@@ -11,20 +12,59 @@ def post_to_logstash(data_in_json):
     #requests.post(logstash_http_endpoint, json=data_in_json)
 
 
-keywords_dict = {"Apple":10,"Microsoft":10,"Amazon":10,"Google":10, "Facebook":10,"riotgames":10,"Samsung":4,"SamsungMobile":10,"nvidia":1,"NVIDIAGeForce":10,"Adobe":2, "Photoshop":5,"Lightroom":2,"creativecloud":1,"AdobeCare":5,"salesforce":2, "tableau":1,"tableauPublic":1,"SlackHQ":3,"MuleSoft":1,"heroku":1,"marketingcloud":2,"asksalesforce":1,"herokustatus":1}
-company_files_dict = {"apple":["Apple"],"microsoft":["Microsoft"],"amazon":["Amazon"],"google":["Google"],"facebook":["Facebook"],"tencent":["riotgames"],"samsung":["Samsung","SamsungMobile"],"nvidia":["nvidia","NVIDIAGeForce"],"adobe":["Adobe","Photoshop","Lightroom","creativecloud","AdobeCare"],"salesforce":["salesforce","tableau","tableauPublic","SlackHQ","MuleSoft","heroku","marketingcloud","asksalesforce","herokustatus"]}
+keywords_dict = {
+    "Apple": 10,
+    "Microsoft": 10,
+    "Amazon": 10,
+    "Google": 10,
+    "Facebook": 10,
+    "riotgames": 10,
+    "Samsung": 4,
+    "SamsungMobile": 10,
+    "nvidia": 1,
+    "NVIDIAGeForce": 10,
+    "Adobe": 2,
+    "Photoshop": 5,
+    "Lightroom": 2,
+    "creativecloud": 1,
+    "AdobeCare": 5,
+    "salesforce": 2,
+    "tableau": 1,
+    "tableauPublic": 1,
+    "SlackHQ": 3,
+    "MuleSoft": 1,
+    "heroku": 1,
+    "marketingcloud": 2,
+    "asksalesforce": 1,
+    "herokustatus": 1
+}
+company_files_dict = {
+    "apple": ["Apple"],
+    "microsoft": ["Microsoft"],
+    "amazon": ["Amazon"],
+    "google": ["Google"],
+    "facebook": ["Facebook"],
+    "tencent": ["riotgames"],
+    "samsung": ["Samsung", "SamsungMobile"],
+    "nvidia": ["nvidia", "NVIDIAGeForce"],
+    "adobe": ["Adobe", "Photoshop", "Lightroom", "creativecloud", "AdobeCare"],
+    "salesforce": [
+        "salesforce", "tableau", "tableauPublic", "SlackHQ", "MuleSoft",
+        "heroku", "marketingcloud", "asksalesforce", "herokustatus"
+    ]
+}
 
 for company in company_files_dict:
     try:
-        print("Sending..."+company)
+        print("Sending..." + company)
         files_list = company_files_dict[company]
         companies_list = list(company_files_dict.keys())
         #cleaned_data_directory = 'CLEANED_DATA/'
-    
-        file_name ="{}_cleaned.json".format(company)
 
-        f = open(cleaned_data_directory+file_name,)
-        fDict = json.load(f) 
+        file_name = "{}_cleaned.json".format(company)
+
+        f = open(cleaned_data_directory + file_name, )
+        fDict = json.load(f)
         f.close()
 
         for entry in fDict["data"]:
@@ -33,4 +73,3 @@ for company in company_files_dict:
     except Exception as e:
         print("[twitterCrawler]: Error in postData.py!")
         print(e)
-
--- twitter crawler/twitterCrawler.py
+++ twitter crawler/twitterCrawler.py
@@ -1,5 +1,3 @@
-
-
 #TODO  rate limit handling.
 #refer to https://towardsdatascience.com/twitter-data-collection-tutorial-using-python-3267d7cfa93e
 import requests
@@ -8,8 +6,8 @@ import json
 import configparser
 
 #fill in the FULL paths before running!
-raw_data_directory = "C:/Users/whate/Documents/AY2021S2/CZ4034 Information Retrieval/twitter stuff/TECH_DATA_2/" #where to save raw data
-directory_secrets = "C:/Users/whate/Documents/AY2021S2/CZ4034 Information Retrieval/twitter stuff/" #contains twitterConfig.ini
+raw_data_directory = "C:/Users/whate/Documents/AY2021S2/CZ4034 Information Retrieval/twitter stuff/TECH_DATA_2/"  #where to save raw data
+directory_secrets = "C:/Users/whate/Documents/AY2021S2/CZ4034 Information Retrieval/twitter stuff/"  #contains twitterConfig.ini
 
 
 def create_headers(bearer_token):
@@ -17,15 +15,14 @@ def create_headers(bearer_token):
     return headers
 
 
-def create_url(keyword,maxResults):
+def create_url(keyword, maxResults):
     query = "{} -is:retweet lang:en".format(keyword)
     tweet_fields = "tweet.fields=author_id,public_metrics,created_at"
     url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&max_results={}".format(
-        query, tweet_fields,maxResults
-    )
+        query, tweet_fields, maxResults)
 
-    
     return url
+
 
 def connect_to_endpoint(url, headers):
     response = requests.request("GET", url, headers=headers)
@@ -34,71 +31,96 @@ def connect_to_endpoint(url, headers):
         raise Exception(response.status_code, response.text)
     return response.json()
 
-def start_companies_crawls(company_files_dict,raw_data_directory,headers):
-        for company in company_files_dict.keys():
-            company_data = {"data":[]}
-            for keyword in company_files_dict[company]:
-                print("Crawling {} for [{}]...".format(keyword,company))
-                current_data =  crawlKeyword(keyword,headers)
-                company_data["data"].extend(current_data)
-            print("-"*30)
-            print("No. Records Crawled for "+company+" is "+str(len(company_data["data"])))
-            with open('{}{}Data.json'.format(raw_data_directory,company), 'w') as f:
-                    json.dump(company_data,f)
 
-def crawlKeyword(keyword,headers):
-        first_flag = True
-        next_token = "0"
-        json_response = {}
-        max_results = 100 #per call(page)
-        num_pages = 10 
-        data_collect = [] #collects the pages of data crawled into a list
-        url = create_url("to:"+keyword,max_results) #remove the "to:" to search for hashtags instead
-        
-        for i in range(num_pages):
-            if(first_flag):
-                next_url = url
-                first_flag = False
-            else:
-                next_token = json_response["meta"]["next_token"]
-                next_url = url+'&next_token='+next_token
-            
-            try:
-                json_response = connect_to_endpoint(next_url,headers)
-            except requests.exceptions.RequestException as e:
-                print("crawlKeyword ERROR!")
+def start_companies_crawls(company_files_dict, raw_data_directory, headers):
+    for company in company_files_dict.keys():
+        company_data = {"data": []}
+        for keyword in company_files_dict[company]:
+            print("Crawling {} for [{}]...".format(keyword, company))
+            current_data = crawlKeyword(keyword, headers)
+            company_data["data"].extend(current_data)
+        print("-" * 30)
+        print("No. Records Crawled for " + company + " is " +
+              str(len(company_data["data"])))
+        with open('{}{}Data.json'.format(raw_data_directory, company),
+                  'w') as f:
+            json.dump(company_data, f)
 
-            meta_part = json_response["meta"]
-            data_collect.extend(json_response["data"])
-            
-            if("next_token" not in meta_part):
-                break
-            else:
-                next_token = meta_part["next_token"]
-            
-        
-        return data_collect    
+
+def crawlKeyword(keyword, headers):
+    first_flag = True
+    next_token = "0"
+    json_response = {}
+    max_results = 100  #per call(page)
+    num_pages = 10
+    data_collect = []  #collects the pages of data crawled into a list
+    url = create_url(
+        "to:" + keyword,
+        max_results)  #remove the "to:" to search for hashtags instead
+
+    for i in range(num_pages):
+        if (first_flag):
+            next_url = url
+            first_flag = False
+        else:
+            next_token = json_response["meta"]["next_token"]
+            next_url = url + '&next_token=' + next_token
+
+        try:
+            json_response = connect_to_endpoint(next_url, headers)
+        except requests.exceptions.RequestException as e:
+            print("crawlKeyword ERROR!")
+
+        meta_part = json_response["meta"]
+        data_collect.extend(json_response["data"])
+
+        if ("next_token" not in meta_part):
+            break
+        else:
+            next_token = meta_part["next_token"]
+
+    return data_collect
+
+
 def main():
     try:
 
         #read in Twitter API token
         config = configparser.ConfigParser()
-        config.read(directory_secrets+'twitterConfig.ini')
+        config.read(directory_secrets + 'twitterConfig.ini')
         secrets = config["secrets"]
-        bearer_token = secrets["bearerToken"] 
+        bearer_token = secrets["bearerToken"]
         headers = create_headers(bearer_token)
 
         #1.1. use this dict for actual crawls
-        company_files_dict = {"apple":["Apple"],"microsoft":["Microsoft"],"amazon":["Amazon"],"google":["Google"],"facebook":["Facebook"],"tencent":["riotgames"],"samsung":["Samsung","SamsungMobile"],"nvidia":["nvidia","NVIDIAGeForce"],"adobe":["Adobe","Photoshop","Lightroom","creativecloud","AdobeCare"],"salesforce":["salesforce","tableau","tableauPublic","SlackHQ","MuleSoft","heroku","marketingcloud","asksalesforce","herokustatus"]}
-        
+        company_files_dict = {
+            "apple": ["Apple"],
+            "microsoft": ["Microsoft"],
+            "amazon": ["Amazon"],
+            "google": ["Google"],
+            "facebook": ["Facebook"],
+            "tencent": ["riotgames"],
+            "samsung": ["Samsung", "SamsungMobile"],
+            "nvidia": ["nvidia", "NVIDIAGeForce"],
+            "adobe":
+            ["Adobe", "Photoshop", "Lightroom", "creativecloud", "AdobeCare"],
+            "salesforce": [
+                "salesforce", "tableau", "tableauPublic", "SlackHQ",
+                "MuleSoft", "heroku", "marketingcloud", "asksalesforce",
+                "herokustatus"
+            ]
+        }
+
         #1.2. use this minimal dict for test crawls
-        #company_files_dict = {"salesforce":["tableau","salesforce"],"adobe":["creativecloud"]} 
-        
+        #company_files_dict = {"salesforce":["tableau","salesforce"],"adobe":["creativecloud"]}
+
         #1.3.uncomment this to start crawling all keywords from the company_file_dict, this saves the data into the path provided
-        start_companies_crawls(company_files_dict,raw_data_directory,headers)
-        
+        start_companies_crawls(company_files_dict, raw_data_directory, headers)
+
     except Exception as e:
         print("[twitterCrawler]: Error in twitterCrawler.py!")
         print(e)
+
+
 if __name__ == "__main__":
     main()
